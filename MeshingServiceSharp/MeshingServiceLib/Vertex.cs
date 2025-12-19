0711758e402a7e36283342113861a0a1f86d9298
Pavel Zhukov <pavel.zhukov.engineer@gmail.com>
2025-12-19 14:36:23 +0200

    add circle

---
+++ MeshingServiceSharp/MeshingServiceLib/Vertex.cs
@@ -0,0 +1,8 @@
+﻿namespace MeshingServiceLib
+{
+    public sealed class Vertex(double x, double y)
+    {
+        public double X { get; set; } = x;
+        public double Y { get; set; } = y;
+    }
+}
