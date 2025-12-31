e4b4b91b00ca9150bd5a50a9e047f5ef7b4f1085
adfgl <69821550+adfgl@users.noreply.github.com>
2025-12-31 14:13:03 +0200

    Update DtoEdge.cs

--- MeshingServiceSharp/TriUgla.WebApi/DtoEdge.cs
+++ MeshingServiceSharp/TriUgla.WebApi/DtoEdge.cs
@@ -1,8 +1,9 @@
 public sealed class DtoEdge
 {
     public string? Id { get; set; }
-    public int Vertex { get; set; }
-    public int Triangle { get; set; }
-    public int Next { get; set; }
-    public int Previous { get; set; }
+    public int Vertex { get; set; } = -1;
+    public int Triangle { get; set; } = -1;
+    public int Next { get; set; } = -1;
+    public int Previous { get; set; } = -1;
+    public int Twin { get; set; } = -1;
 }
