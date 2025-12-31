84d043ef7b1f44c10c644ee4f554c301109e4778
adfgl <69821550+adfgl@users.noreply.github.com>
2025-12-31 13:03:53 +0200

    Create Interpolation.cs

---
+++ MeshingServiceSharp/TriUgla.Mesher/Interpolation.cs
@@ -0,0 +1,46 @@
+public static class Interpolation
+{
+    public static readonly double ZAtXYAlongSegment(
+        Vertex v0, Vertex v1, double x, double y)
+    {
+        double dx = v1.x - v0.x;
+        double dy = v1.y - v0.y;
+
+        double ddx = v1.x - x;
+        double ddy = v1.y - y;
+
+        double k = Math.Sqrt(
+            (ddx * ddx + ddy * ddy) /
+            (dx * dx + dy * dy));
+
+        return v0.z + (v1.z - v0.z) * k;
+    }
+
+    public static readonly double ZAtXYInTriangle(
+        Vertex v0, Vertex v1, Vertex v2,
+        double x, double y)
+    {
+        double e0x = v1.x - v0.x;
+        double e0y = v1.y - v0.y;
+
+        double e1x = v2.x - v0.x;
+        double e1y = v2.y - v0.y;
+
+        double px  = x - v0.x;
+        double py  = y - v0.y;
+
+        double denom = GeometryHelper.Cross(e0x, e0y, e1x, e1y);
+
+#if DEBUG
+        if (Math.Abs(denom) < 1e-30)
+            throw new InvalidOperationException("Triangle is degenerate in XY.");
+#endif
+
+        double w1 = GeometryHelper.Cross(px, py, e1x, e1y) / denom; // weight at v1
+        double w2 = GeometryHelper.Cross(e0x, e0y, px, py) / denom; // weight at v2
+        double w0 = 1.0 - w1 - w2;
+
+        return w0 * v0.z + w1 * v1.z + w2 * v2.z;
+    }
+
+}
