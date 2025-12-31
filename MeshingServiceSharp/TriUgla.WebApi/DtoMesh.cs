f59b5f87e24c7293e543d4c6dbefdd13f5933d40
adfgl <69821550+adfgl@users.noreply.github.com>
2025-12-31 14:48:02 +0200

    Update DtoMesh.cs

--- MeshingServiceSharp/TriUgla.WebApi/DtoMesh.cs
+++ MeshingServiceSharp/TriUgla.WebApi/DtoMesh.cs
@@ -3,4 +3,22 @@ public sealed class DtoMesh
     public List<DtoVertex> Vertices { get; set; }
     public List<DtoEdge> Edges { get; set; }
     public List<DtoFace> Faces { get; set; }
+    public DtoMeshMeta Meta { get; set; }
+}
+
+public sealed class DtoMeshMeta
+{
+    public DtoMeshMetric EdgeLength { get; set; }
+    public DtoMeshMetric TriangleArea { get; set; }
+    public DtoMeshMetric TriangleAngle { get; set; }
+}
+
+public sealed class DtoMeshMetric
+{
+    public int Quantity { get; set; }
+    public double Min { get; set; }
+    public int MinIndex { get; set; }
+    public double Max { get; set; }
+    public int MaxIndex { get; set; }
+    public double Average { get; set; }
 }
