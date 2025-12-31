237e1cdb93fa2d0d2e5c6b093897cf31a2563752
adfgl <69821550+adfgl@users.noreply.github.com>
2025-12-31 12:49:05 +0200

    Update Vertex.cs

--- MeshingServiceSharp/TriUgla.Mesher/Vertex.cs
+++ MeshingServiceSharp/TriUgla.Mesher/Vertex.cs
@@ -14,5 +14,22 @@ namespace TriUgla.Mesher
             double dx = a.x - b.x, dy = a.y - b.y;
             return dx * dx + dy * dy <= eps * eps;
         }
+
+        public static readonly double ZAlongLineAtXY(
+            Vertex v0, Vertex v1, double x, double y)
+        {
+            double dx = v1.x - v0.x;
+            double dy = v1.y - v0.y;
+
+            double ddx = v1.x - x;
+            double ddy = v1.y - y;
+
+            double k = Math.Sqrt(
+                (ddx * ddx + ddy * ddy) /
+                (dx * dx + dy * dy));
+
+            return v0.z + (v1.z - v0.z) * k;
+
+        }
     }
 }
