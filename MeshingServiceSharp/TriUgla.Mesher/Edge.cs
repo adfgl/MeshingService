517f4b493e3aa7579ba08f56da52a225953c0ee4
adfgl <69821550+adfgl@users.noreply.github.com>
2026-01-02 10:39:48 +0200

    Update Edge.cs

--- MeshingServiceSharp/TriUgla.Mesher/Edge.cs
+++ MeshingServiceSharp/TriUgla.Mesher/Edge.cs
@@ -1,7 +1,32 @@
-﻿namespace TriUgla.Mesher
+namespace TriUgla.Mesher
 {
-    public readonly struct Edge(int start, int end)
+    public readonly struct Edge(
+        int start, int end,
+        int triangle = -1, string? id = null)
     {
         public readonly int start = start, end = end;
+        public readonly int triangle = triangle;
+        public readonly string? id;
+
+        public readonly static void Split(
+            in Edge edge, 
+            int vertex, 
+            int triangle1,
+            int triangle2,
+            out Edge edge1,
+            out Edge edge2)
+        {
+            edge1 = new Edge(
+                edge.start, 
+                vertex, 
+                triangle1,
+                edge.id);
+
+            edge2 = new Edge(
+                vertex,
+                edge.end,
+                triangle2,
+                edge.id);
+        }
     }
 }
