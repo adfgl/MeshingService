92e6f575a7cf02dd70bb825dc0eff8a3ec08f7c3
adfgl <69821550+adfgl@users.noreply.github.com>
2026-01-05 14:03:06 +0200

    Update ShapePreprocessor.cs

--- MeshingServiceSharp/TriUgla.Mesher/ShapePreprocessor.cs
+++ MeshingServiceSharp/TriUgla.Mesher/ShapePreprocessor.cs
@@ -1,7 +1,12 @@
 public sealed class ShapePreprocessor
 {
-    public void Do()
+    public void Do(Shape shape)
     {
+        List<Vertex> vertices = new List<Vertex>();
+    }
 
+    public static int GetOrAdd(List<Vertex> existing, in Vertex vtx, double eps)
+    {
+        foreach 
     }
 }
