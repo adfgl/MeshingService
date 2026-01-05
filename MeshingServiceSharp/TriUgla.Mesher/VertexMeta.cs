4329f0eff06b5e1c1a542ff90ab40c16dcd1014e
adfgl <69821550+adfgl@users.noreply.github.com>
2026-01-05 14:08:53 +0200

    Delete VertexMeta.cs

--- MeshingServiceSharp/TriUgla.Mesher/VertexMeta.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TriUgla.Mesher
-{
-    public struct VertexMeta(int triangle, string? id = null)
-    {
-        public string? id = id;
-        public int triangle = triangle;
-    }
-}
